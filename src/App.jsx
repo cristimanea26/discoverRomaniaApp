27321c460092e3759776572a00398088d773c1d8
unknown <cristimanea26@gmail.com>
2023-05-03 13:20:53 +0300

    update08

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import DiscoverRomania from './components/DiscoverRomania.jsx';
+import React from "react";
+import DiscoverRomania from "./components/DiscoverRomania";
 
 const App = () => {
   return (
-    <div className='App'>
-      <div className='content'>
+    <div className="App">
+      <div className="content">
         <DiscoverRomania />
       </div>
     </div>
--- src/components/DiscoverRomania.jsx
+++ src/components/DiscoverRomania.jsx
@@ -1,36 +1,36 @@
-import React from 'react';
-import { motion as m } from 'framer-motion';
-import { fadeIn } from './Animations.jsx';
+import React from "react";
+import { motion as m } from "framer-motion";
+import { fadeIn } from "./Animations";
 
 const DiscoverRomania = () => {
   return (
-    <div className='discoverRomania'>
+    <div className="discoverRomania">
       <m.div
-        className='dRBanner'
-        variants={fadeIn('down', 'tween', 0.75, 1)}
-        initial='hidden'
-        animate='show'
+        className="dRBanner"
+        variants={fadeIn("down", "tween", 0.75, 1)}
+        initial="hidden"
+        animate="show"
       >
         <m.div
-          className='dRBannerTitle'
-          variants={fadeIn('up', 'tween', 1.5, 2)}
-          initial='hidden'
-          animate='show'
+          className="dRBannerTitle"
+          variants={fadeIn("up", "tween", 1.5, 2)}
+          initial="hidden"
+          animate="show"
         >
           <h2>DISCOVER</h2>
           <h1>ROMANIA</h1>
           <p>This project was made using the following design:</p>
           <a
-            href='https://www.figma.com/file/kWATAszSANhAXBRQX3f6lj/Visit-Romania-parallax'
-            target='_blank'
+            href="https://www.figma.com/file/kWATAszSANhAXBRQX3f6lj/Visit-Romania-parallax"
+            target="_blank"
           >
             Discover Romania
           </a>
         </m.div>
       </m.div>
-      <div className='dRInfo'>
-        <div className='dRInfoContainer'>
-          <div className='dRInfoInner'>
+      <div className="dRInfo">
+        <div className="dRInfoContainer">
+          <div className="dRInfoInner">
             <h1>Explore the nature</h1>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi non
@@ -46,27 +46,27 @@ const DiscoverRomania = () => {
               malesuada.
             </p>
           </div>
-          <div className='dRInfoImg'>
-            <div className='dRInfoImgInner'>
-              <a href='#explorethenature'>Explore the nature</a>
+          <div className="dRInfoImg">
+            <div className="dRInfoImgInner">
+              <a href="#explorethenature">Explore the nature</a>
             </div>
           </div>
         </div>
       </div>
-      <div className='dRLandmarks'>
-        <div className='dRLandmarksContainer'>
-          <div className='dRLandmarksInner countryside'>
-            <a href='#countryside'>COUNTRYSIDE</a>
+      <div className="dRLandmarks">
+        <div className="dRLandmarksContainer">
+          <div className="dRLandmarksInner countryside">
+            <a href="#countryside">COUNTRYSIDE</a>
           </div>
         </div>
-        <div className='dRLandmarksContainer'>
-          <div className='dRLandmarksInner cities'>
-            <a href='#cities'>CITIES</a>
+        <div className="dRLandmarksContainer">
+          <div className="dRLandmarksInner cities">
+            <a href="#cities">CITIES</a>
           </div>
         </div>
-        <div className='dRLandmarksContainer'>
-          <div className='dRLandmarksInner landmarks'>
-            <a href='#landmarks'>LANDMARKS</a>
+        <div className="dRLandmarksContainer">
+          <div className="dRLandmarksInner landmarks">
+            <a href="#landmarks">LANDMARKS</a>
           </div>
         </div>
       </div>
